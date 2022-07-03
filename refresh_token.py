2cea3b09e1dd6317c4dd6811787e8a75916fb89f
donrkabob <Mystery>
2022-07-02 19:07:53 -0700

    getting ready for initial push

--- refresh_token.py
+++ refresh_token.py
@@ -3,31 +3,7 @@ import sys
 import json
 import argparse
 import tda
-
-class Config(object):
-    def __init__(self):
-        self.apikey = None
-        self.callbackuri = None
-        self.accountnum = None
-        self.tokenpath = None
-
-    def read_config(self, config_file):
-        #print("Reading config")
-        fh = open(config_file, 'r')
-        c = json.load(fh)
-        fh.close()
-        self.apikey = c["apikey"]
-        self.callbackuri = c["callbackuri"]
-        self.tokenpath = c['tokenpath']
-        self.accountnum = c['accountnum']
-
-    def write_config(self, cfile):
-        fh = open(cfile, 'w')
-        fh.write(json.dumps(self.__dict__))
-        fh.close()
-
-    def __str__(self):
-        return json.dumps(self.__dict__, indent=4)
+from datastructures import Config
 
 def setup(conf: Config, tokenpath, cfile):
     cf = {}
@@ -53,14 +29,17 @@ if __name__ == '__main__':
     print("Let's get a refresh token")
     ap = argparse.ArgumentParser()
     #ap.add_argument("--newtoken", action="store_true", dest='newtoken', default=False)
-    ap.add_argument("--tdaconfig", dest="tdaconfig", default="refresh.json")
+    ap.add_argument("--tdaconfig", dest="tdaconfig", default="tda-config.json")
+    ap.add_argument("--nosetup", dest="nosetup", default=False, action="store_true")
     ap.add_argument("--configfile", dest='configfile', default="config.json")
     args = vars(ap.parse_args())
     c = Config()
-    if args['configfile'] is not None:
+    if ap['nosetup'] is False:
+        setup(c, args['tdaconfig'], None)
+    elif args['configfile'] is not None:
         c.read_config(args['configfile'])
     else:
-        setup(c, args['tdaconfig'], None)
+        raise FileNotFoundError("Config file provided is None and \"--nosetup\" was specified")
     client = setup_client(c)
     res = client.get_account(c.accountnum)
-    sys.exit()
+    sys.exit(0)
