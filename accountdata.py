ed5377ecc076eb3619090c99fd03ca48714dd736
donrkabob <Mystery>
2022-07-02 19:07:56 -0700

    getting ready for initial push

--- accountdata.py
+++ accountdata.py
@@ -1,4 +1,7 @@
 import json
+
+import pandas as pd
+
 from datastructures import Config
 from tda.client import Client as TDAclient
 
@@ -6,6 +9,16 @@ from tda.client import Client as TDAclient
 class AccountData(object):
 
     def __init__(self):
+        self.nlv = 0
+        self.bp = 0
+        self.bp_available = 0
+        self.bpu = 0
+        self.starting_nlv = 0
+        self.starting_bp = 0
+        self.starting_bpu = 0.0
+        self.sutmax = 0
+
+    def reset(self):
         self.nlv = 0
         self.bp = 0
         self.bp_available = 0
@@ -28,5 +41,15 @@ class AccountData(object):
         self.starting_bpu = round(self.starting_bpu * 100, 2)
 
         self.sutmax = int((self.nlv / float(1000))) * 5
-        # print(json.dumps(acc, indent=4))
-        # print(json.dumps(adata, indent=4))
+
+    def to_dataframe(self, index_label: str = "Stats"):
+        pass
+
+def get_display_df(ad: AccountData, index: list =["Stats"]):
+    d = {}
+    d['NLV'] = ad.nlv
+    d['BPu'] = ad.bpu
+    d['Buying Power'] = ad.bp_available
+    d['Short Unit Max'] = ad.sutmax
+    df = pd.DataFrame(d, index=index)
+    return df
